88a235e93b82d18092caa148dc161444c577fcb1
Wenhao Chen <xjtujack@gmail.com>
2017-04-04 15:22:32 -0500

    Update Settings.java

--- src/common/Settings.java
+++ src/common/Settings.java
@@ -5,4 +5,5 @@ public class Settings {
 	public static final String avdDir = "C:/Users/guan-local.CO3223-02/.android/avd";
 	public static final String adbPath = 	  "C:/Users/guan-local.CO3223-02/AppData/Local/Android/SDK/platform-tools/adb.exe";
 	public static final String emulatorPath = "C:/Users/guan-local.CO3223-02/AppData/Local/Android/SDK/tools/emulator.exe";
+	public static final String outputDir = "out/";
 }
